9772e7f75614297770564d8f949149b9610a918c
Kwaku Adusei Okyere <32445365+asteroid2k@users.noreply.github.com>
2022-03-28 10:45:10 +0000

    husky config

--- src/routes/v1/index.ts
+++ src/routes/v1/index.ts
@@ -3,7 +3,9 @@ import userRouter from "./user.routes";
 import authRouter from "./auth.routes";
 
 const router: Router = Router();
-
+router.get("/", (req, res) => {
+  res.send("Hello");
+});
 router.use("/auth", authRouter);
 router.use("/users", userRouter);
 
